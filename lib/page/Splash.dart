176fff33e183248665cd7efa9adf08fef0f36b87
Narukarudra09 <160457532+Narukarudra09@users.noreply.github.com>
2024-12-07 21:31:09 +0530

    splash added

--- lib/page/Splash.dart
+++ lib/page/Splash.dart
@@ -13,14 +13,17 @@ class _SplashState extends State<Splash> {
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 2), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => const Home(),
-        ),
-      );
-    });
+    Timer(
+      const Duration(seconds: 2),
+      () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const Home(),
+          ),
+        );
+      }
+    );
   }
 
   @override
@@ -28,7 +31,11 @@ class _SplashState extends State<Splash> {
     return Material(
       color: const Color.fromRGBO(0, 57, 119, 1.0),
       child: Center(
-        child: Image.asset("assets/image/logo.png",width: 200,height: 200,),
+        child: Image.asset(
+          "assets/image/logo.png",
+          width: 200,
+          height: 200,
+        ),
       ),
     );
   }
