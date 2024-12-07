6da3fb06e292b55bec04597e58d537ed83ae2d5d
Narukarudra09 <160457532+Narukarudra09@users.noreply.github.com>
2024-12-07 21:10:24 +0530

    splash added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:indian_public_school/page/Splash.dart';
 import 'package:indian_public_school/page/home.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Home(),
+      home: Splash(),
     );
   }
 }
---
+++ lib/page/Splash.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:indian_public_school/page/home.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const Home(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: const Color.fromRGBO(0, 57, 119, 1.0),
+      child: Center(
+        child: Image.asset("assets/image/logo.png",width: 200,height: 200,),
+      ),
+    );
+  }
+}
